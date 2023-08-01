adc92299d4b35e458a26d139d1976b9a6612e4b4
iam-anuroop <anuroopku10@gmail.com>
2023-08-01 12:15:59 +0530

    static

--- C_zone/settings.py
+++ C_zone/settings.py
@@ -196,9 +196,16 @@ RAZOR_KEY_SECRET = os.getenv('razor_key_secret')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'czonebucket'
-AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STORAGES = {
+            "staticfiles": {
+                "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+            },
+            "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage", 
+            },
+            }
